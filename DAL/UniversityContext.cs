63d64f562e45cfcd739efeaaa813360d615928eb
Josesanchez152100 <70731465+Josesanchez152100@users.noreply.github.com>
2020-12-13 18:09:20 -0500

    Initial commit

---
+++ DAL/UniversityContext.cs
@@ -0,0 +1,26 @@
+﻿using CesdeUniversityJS.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace CesdeUniversityJS.DAL
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext() : base("UniversityContext")
+        {
+        }
+
+        public DbSet<Student> Students{ get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
